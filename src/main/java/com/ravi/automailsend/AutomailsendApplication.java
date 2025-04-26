eaacd183239f3a4af0af0808a84a4fda237b22d2
Raviteja Gandam <gandamraviteja@gmail.com>
2025-04-26 16:19:33 +0530

    First commit

---
+++ src/main/java/com/ravi/automailsend/AutomailsendApplication.java
@@ -0,0 +1,13 @@
+package com.ravi.automailsend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomailsendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomailsendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ravi/automailsend/AutomailsendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravi.automailsend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomailsendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
